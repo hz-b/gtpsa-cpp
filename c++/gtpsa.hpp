3a99604fcbae2974400690d372f38ede2ba37245
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2022-09-14 14:19:15 +0200

    gtpsa: added trigonometric and similar functions to the wrapper

--- c++/gtpsa.hpp
+++ c++/gtpsa.hpp
@@ -23,14 +23,15 @@ namespace gtpsa {
 
     class tpsa_mgr {
     public:
-	inline tpsa_mgr(const tpsa_mgr&& o)      { this->ptr = o.ptr;  };
-	inline tpsa_mgr(tpsa_t *p) {
-	    this->ptr = p;
-	}
 	inline ~tpsa_mgr(void){
 	    mad_tpsa_del(this->ptr);
 	    this->ptr = nullptr; // a bit of paranoia
 	}
+	inline tpsa_mgr(tpsa_t *p) {
+	    this->ptr = p;
+	}
+    private:
+	inline tpsa_mgr(const tpsa_mgr&& o)      { this->ptr = o.ptr;  };
 	tpsa_mgr(const tpsa_mgr& o) = delete;
 
     private:
@@ -40,6 +41,13 @@ namespace gtpsa {
 
 	friend class tpsa;
     };
+
+    // forward declaration ... required that operators will use the same functions
+    inline void add (const tpsa& a, const tpsa& b, tpsa* r);
+    inline void sub (const tpsa& a, const tpsa& b, tpsa* r);
+    inline void mul (const tpsa& a, const tpsa& b, tpsa* r);
+    inline void div (const tpsa& a, const tpsa& b, tpsa* r);
+    inline void pow (const tpsa& a, const tpsa& b, tpsa* r);
 
 class tpsa{
 public:
@@ -114,18 +122,18 @@ public:
     /*
      * @todo: use standard accessor operators ?
      */
-    inline void getv(idx_t i, std::vector<num_t> &v) { mad_tpsa_setv(this->getPtr(), i, v.size(), v.data());   }
+    inline void getv(idx_t i, std::vector<num_t> &v) { mad_tpsa_getv(this->getPtr(), i, v.size(), v.data());   }
     inline void setv(idx_t i, std::vector<num_t> *v) { mad_tpsa_setv(this->getPtr(), i, v->size(), v->data()); }
 
     inline void print(str_t name_, num_t eps_, int nohdr_, FILE *stream_){
 	mad_tpsa_print(this->getPtr(), name_, eps_, nohdr_, stream_);
     }
 
-    inline tpsa& operator += (const tpsa& o){ mad_tpsa_add(this->getPtr(), o.getPtr(), this->getPtr()); return *this;}
+    inline tpsa& operator += (const tpsa& o){ add(*this, o, this); return *this;}
     // (a_i-b_i)/max(|a_i|,1)
-    inline tpsa& operator -= (const tpsa& o){ mad_tpsa_sub(this->getPtr(), o.getPtr(), this->getPtr()); return *this;}
-    inline tpsa& operator *= (const tpsa& o){ mad_tpsa_mul(this->getPtr(), o.getPtr(), this->getPtr()); return *this;}
-    inline tpsa& operator /= (const tpsa& o){ mad_tpsa_div(this->getPtr(), o.getPtr(), this->getPtr()); return *this;}
+    inline tpsa& operator -= (const tpsa& o){ sub(*this, o, this); return *this;}
+    inline tpsa& operator *= (const tpsa& o){ mul(*this, o, this); return *this;}
+    inline tpsa& operator /= (const tpsa& o){ div(*this, o, this); return *this;}
 
 private:
     std::unique_ptr<tpsa_mgr>  tm;
@@ -133,22 +141,15 @@ private:
     inline const tpsa_t * getPtr(void) const { return this->tm->getPtr(); }
     inline tpsa_t *       getPtr(void)       { return this->tm->getPtr(); }
 
+    friend inline void process2_   (const tpsa& a, const tpsa& b, tpsa *r,  void (*func)(const tpsa_t* a, const tpsa_t* b, tpsa_t *r)  );
+    friend inline void process1_   (const tpsa& a,                tpsa *r,  void (*func)(const tpsa_t* a,                  tpsa_t *r)  );
+    friend inline void process1to2_(const tpsa& a,      tpsa* r1, tpsa *r2, void (*func)(const tpsa_t* a,       tpsa_t *r, tpsa_t *r2) );
+    //friend void inline process_op2_(const tpsa& a, const tpsa& b, tpsa *r, void (*func)(const tpsa& a, const tpsa& b, tpsa *r) );
+
     friend inline auto norm (const tpsa& a);
 
-    friend inline void add  (const tpsa& a, const tpsa& b, tpsa *r);
-    friend inline void sub  (const tpsa& a, const tpsa& b, tpsa *r);
-    friend inline void mul  (const tpsa& a, const tpsa& b, tpsa *r);
-    friend inline void div  (const tpsa& a, const tpsa& b, tpsa *r);
-
-    friend inline void pow  (const tpsa& a, const tpsa& b, tpsa *r);
     friend inline void pow  (const tpsa& a,         int n, tpsa *r);
     friend inline void pow  (const tpsa& a,       num_t v, tpsa *r);
-
-    friend inline void asin_(const tpsa& t1, tpsa *t2);
-    friend inline tpsa asin (const tpsa& t1);
-    friend inline void sin_ (const tpsa& t1, tpsa *t2);
-    friend inline tpsa sin  (const tpsa& t1);
-
     friend inline auto ordn(std::vector<tpsa *> objs);
 
 }; // class tpsa
@@ -160,33 +161,64 @@ private:
 	return mad_tpsa_ordn(tv.size(), tv.data());
     }
 
+    inline void process1_   (const tpsa& a, tpsa *r,
+			     void (*func)(const tpsa_t* a,tpsa_t *r) ) {
+	func(a.getPtr(), r->getPtr());
+    }
+    inline void process1to2_(const tpsa& a, tpsa* r1, tpsa *r2,
+			     void (*func)(const tpsa_t* a, tpsa_t *r1,  tpsa_t *r2) ) {
+	func(a.getPtr(), r1->getPtr(), r2->getPtr());
+    }
+    inline void process2_(const tpsa& a, const tpsa& b, tpsa *r,
+			  void (*func)(const tpsa_t* a, const tpsa_t* b, tpsa_t *r) ) {
+	func(a.getPtr(), b.getPtr(), r->getPtr());
+    }
+
+    tpsa inline process1(const tpsa& t, void (*func)(const tpsa& t, tpsa *r) ) {auto ret = tpsa(t, mad_tpsa_same); func(t, &ret); return std::move(ret);}
+
     inline auto norm(const tpsa& a)                        { return mad_tpsa_nrm(a.getPtr()); }
+    inline void add (const tpsa& a, const tpsa& b, tpsa* r){ process2_(a, b, r, mad_tpsa_add); }
+    inline void sub (const tpsa& a, const tpsa& b, tpsa* r){ process2_(a, b, r, mad_tpsa_sub); }
+    inline void mul (const tpsa& a, const tpsa& b, tpsa* r){ process2_(a, b, r, mad_tpsa_mul); }
+    inline void div (const tpsa& a, const tpsa& b, tpsa* r){ process2_(a, b, r, mad_tpsa_div); }
+    inline void pow (const tpsa& a, const tpsa& b, tpsa* r){ process2_(a, b, r, mad_tpsa_pow); }
 
-    inline void add (const tpsa& a, const tpsa& b, tpsa *r){ mad_tpsa_add (a.getPtr(), b.getPtr(), r->getPtr()); }
-    inline void sub (const tpsa& a, const tpsa& b, tpsa *r){ mad_tpsa_sub (a.getPtr(), b.getPtr(), r->getPtr()); }
-    inline void mul (const tpsa& a, const tpsa& b, tpsa *r){ mad_tpsa_mul (a.getPtr(), b.getPtr(), r->getPtr()); }
-    inline void div (const tpsa& a, const tpsa& b, tpsa *r){ mad_tpsa_div (a.getPtr(), b.getPtr(), r->getPtr()); }
-
-    inline void pow (const tpsa& a, const tpsa& b, tpsa *r){ mad_tpsa_pow (a.getPtr(), b.getPtr(), r->getPtr()); }
-    inline void pow (const tpsa& a,         int n, tpsa *r){ mad_tpsa_powi(a.getPtr(), n,          r->getPtr()); }
-    inline void pow (const tpsa& a,       num_t v, tpsa *r){ mad_tpsa_pown(a.getPtr(), v,          r->getPtr()); }
-
+    inline void pow (const tpsa& a,         int n, tpsa* r){ mad_tpsa_powi(a.getPtr(), n, r->getPtr()); }
+    inline void pow (const tpsa& a,       num_t v, tpsa* r){ mad_tpsa_pown(a.getPtr(), v, r->getPtr()); }
 
 
-    // test the interface ...
-    void inline asin_(const tpsa& t1, tpsa *t2){ mad_tpsa_asin(t1.getPtr(), t2->getPtr()); }
-    void inline sin_ (const tpsa& t1, tpsa *t2){ mad_tpsa_asin(t1.getPtr(), t2->getPtr()); }
-    tpsa inline asin(const tpsa& t) {
-	auto ret = tpsa(t, mad_tpsa_same);
-	mad_tpsa_asin(t.getPtr(), ret.getPtr());
-	return std::move(ret);
-    }
+    inline void abs_    (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_abs   ); }
+    inline void sqrt_   (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_sqrt  ); }
+    inline void exp_    (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_exp   ); }
+    inline void log_    (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_log   ); }
+    inline void sin_    (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_sin   ); }
+    inline void cos_    (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_cos   ); }
+    inline void tan_    (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_tan   ); }
+    inline void cot_    (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_cot   ); }
+    inline void sinc_   (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_sinc  ); }
+    inline void sinh_   (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_sinh  ); }
+    inline void cosh_   (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_cosh  ); }
+    inline void tanh_   (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_tanh  ); }
+    inline void coth_   (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_coth  ); }
+    inline void sinhc_  (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_sinhc ); }
+    inline void asin_   (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_asin  ); }
+    inline void acos_   (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_acos  ); }
+    inline void atan_   (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_atan  ); }
+    inline void acot_   (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_acot  ); }
+    inline void asinc_  (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_asinc ); }
+    inline void asinh_  (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_asinh ); }
+    inline void acosh_  (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_acosh ); }
+    inline void atanh_  (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_atanh ); }
+    inline void acoth_  (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_acoth ); }
+    inline void asinhc_ (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_asinhc); }
+    inline void erf_    (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_erf   ); }
+    inline void erfc_   (const tpsa& t, tpsa* r)            { process1_(t, r, mad_tpsa_erfc  ); }
 
-    tpsa inline sin(const tpsa& t) {
-	auto ret = tpsa(t, mad_tpsa_same);
-	mad_tpsa_sin(t.getPtr(), ret.getPtr());
-	return std::move(ret);
-    }
+    inline void sincos_ (const tpsa& t, tpsa* r1, tpsa* r2) { process1to2_(t, r1, r2, mad_tpsa_sincos);  }
+    inline void sincosh_(const tpsa& t, tpsa* r1, tpsa* r2) { process1to2_(t, r1, r2, mad_tpsa_sincosh); }
+
+    inline tpsa asin(const tpsa& t) { return process1(t, asin_); }
+    inline tpsa sin (const tpsa& t) { return process1(t, sin_);  }
 
 }; // namespace gtsa
 // --- end --------------------------------------------------------------------o
