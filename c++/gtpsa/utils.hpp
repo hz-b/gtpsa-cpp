11c0af6b84899344187998917c2aa4b305f73a33
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2023-01-17 15:32:42 +0100

    Preparations so that tpsa and double or ctpsa and complex can be used interchangebly.
    Rebased on main
    
    variant implementation:
    c++ / gtpsa
           gtpsa_base_variant: template implementation
           variant_forward_decl: forward declarations (e.g. to
                                 define friend between ctpsa and tpsa)
           tpsa_double_variant:  double or tpsa
           ctpsa_cplx_variant:   complex or ctpsa
    
    tests:
           tpsa/double : test of arithmetic operators
           cptsa/complex: mainly compile tests
    
    ss_vect:
           now only checking that dealing with vectors of the same size
           permitting more than one length
    
    further work
           arithmetic operator for tpsa: tests added
           ctpsa supports .real and .imag to set the different parts
    
           test construction of a std::vector of tpsa objects

--- c++/gtpsa/utils.hpp
+++ c++/gtpsa/utils.hpp
@@ -4,23 +4,27 @@
 #include <gtpsa/desc.hpp>
 #include <gtpsa/utils.hpp>
 
-#include <algorithm>
+#include <algorithm> // to be removed?
+#include <complex>
 
 namespace gtpsa {
     template<class T>
-    inline double cst(T &v) { return v.cst(); }
-
+    inline auto cst(T &v) { return v.cst(); }
+    //inline std::complex<double> cst(gtpsa::ctpsa &v) { return v.get_complex(); }
     inline double cst(double v) { return v; }
+    inline std::complex<double> cst(std::complex<double> v) { return v; }
 
     template<class T>
-    inline T clone(T &v) { return v.clone(); }
+    inline T clone(const T &v) { return v.clone(); }
 
-    inline double clone(double v) { return v; }
+    inline auto clone(const double v) { return v; }
+    inline auto clone(const std::complex<double> v) { return v; }
 
     template<class T>
     inline T same_as_instance(const T &v) { return T(v, init::same); }
 
-    inline double same_as_instance(const double &v) { return double(0e0); }
+    inline auto same_as_instance(const double &v) { return double(0e0); }
+    inline auto same_as_instance(const std::complex<double> &v) { return double(0e0); }
 
 
 } // namespace gtpa
