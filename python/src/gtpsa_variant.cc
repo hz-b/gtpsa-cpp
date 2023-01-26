7955d4dd74b8c6b723f5c4af79d487a2616fa215
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2023-01-26 17:28:19 +0100

    python:    variant ... linking to variant

--- python/src/gtpsa_variant.cc
+++ python/src/gtpsa_variant.cc
@@ -1,3 +1,22 @@
 //
 // Created by mfp on 24.01.23.
 //
+
+#include "gtpsa_module.h"
+#include <pybind11/pybind11.h>
+#include <gtpsa/gtpsa_base_variant.hpp>
+#include <gtpsa/tpsa_double_variant.hpp>
+#include <gtpsa/ctpsa_complex_variant.hpp>
+namespace PYBIND11_NAMESPACE {
+    namespace detail {
+        template <> struct type_caster<gtpsa::TpsaOrDouble>{
+        public:
+            PYBIND11_TYPE_CASTER(gtpsa::TpsaOrDouble, const_name("TpsaOrDouble"));
+        };
+    } // namespace PYBIND11_NAMESPACE::detail
+} // namespace PYBIND11_NAMESPACE
+
+    void py_gtpsa_init_variant(pybind11::module &m)
+{
+
+}
